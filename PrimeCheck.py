f73432520a97199850d6f8775a9d0df889c5290b
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-05-17 00:12:20 +0545

    Checking Primes

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -1,5 +1,5 @@
 a = int(input("Enter Any Number:- "))
 if a == 2 or a == 3 or (( a * a ) - 1 ) % 24 == 0:
-    print("Prime")
+    print("Might be Prime")
 else:
-    print("Not Prime")
+    print("definitely Not Prime")
