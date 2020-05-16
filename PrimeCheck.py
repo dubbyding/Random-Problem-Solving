31a9003521d9fef555fd76f397e5f49fe7458bec
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-05-17 01:34:50 +0545

    Fully completed

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -1,7 +1,18 @@
+import time
+def checkPrime(a):
+    if a == '2' or a == '5':
+        return True
+    elif a[-1] in ('1', '3', '7') and (( int(a) * int(a) ) - 1 ) % 24 == 0: 
+        return(loopcheck(int(a)))
+    else:
+        return False
+def loopcheck(a):
+    for i in range(2, int(a/2) + 1):
+        if a % i == 0:
+            return False
+    return True
 a = input("Enter Any Number:- ")
-if a == '2' or a == '5':
-    print('It is Prime')
-elif a[-1] in ('1', '3', '7') and (( int(a) * int(a) ) - 1 ) % 24 == 0: 
-    print("It Might be Prime")
-else:
-    print("It is not a prime")
+tic = time.time()
+print("Prime") if checkPrime(a) else print("Not Prime")
+toc = time.time()
+print(toc-tic)
