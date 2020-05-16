2a7eda6a59bae6ff3b70d1bfba4c03fba755f03f
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-05-17 00:25:19 +0545

    On 2 Like check

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -1,5 +1,2 @@
 a = int(input("Enter Any Number:- "))
-if a == 2 or a == 3 or (( a * a ) - 1 ) % 24 == 0:
-    print("Might be Prime")
-else:
-    print("definitely Not Prime")
+print("Might be Prime") if a == 2 or a == 3 or (( a * a ) - 1 ) % 24 == 0 else print("definitely Not Prime")
