d059324e5cb9585e97e4f6008b2950f455d1db74
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-05-17 00:50:45 +0545

    More non primes can now be removed

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -1,2 +1,7 @@
-a = int(input("Enter Any Number:- "))
-print("Might be Prime") if a == 2 or a == 3 or (( a * a ) - 1 ) % 24 == 0 else print("definitely Not Prime")
+a = input("Enter Any Number:- ")
+if a == '2' or a == '5':
+    print('It is Prime')
+elif a[-1] in ('1', '3', '7') and (( int(a) * int(a) ) - 1 ) % 24 == 0: 
+    print("It Might be Prime")
+else:
+    print("It is not a prime")
