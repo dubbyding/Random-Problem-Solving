c6890b3e651ccd69fee52b6c32f1fe92f4828a49
Raj Maharjan <rmaharjan.rm90@gmail.com>
2021-01-02 15:22:03 +0545

    Mid-Point Circle Drawing Algorithm Done

--- MPCDA.c
+++ MPCDA.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int display(int counter, int pk, int x, int y, int x1, int y1){
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, x+x1, y+y1);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, y+y1, x+x1);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, x1-x, y+y1);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, y+y1, x1-x);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, x+x1, y1-y);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, y1-y, x+x1);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, x1-x, y1-y);
+    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, y1-y, x1-x);
+    return counter;
+}
+
 void MidPointCircleDrawingAlgorithm(int x1, int y1, int r){
     int x = 0, y = r, counter = 0;
     int pk = 1 - r;
     printf("N\tPk\tx\ty\tx1\ty1\n");
-    printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, x+x1, y+y1);
+    counter = display(counter, pk, x, y, x1, y1);
     do{
         if(pk < 0){
             x = x + 1;
@@ -16,7 +28,9 @@ void MidPointCircleDrawingAlgorithm(int x1, int y1, int r){
             y--;
             pk = pk + 2*x -2*y +1;
         }
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n",counter++, pk, x, y, x+x1, y+y1);
+        
+        counter = display(counter, pk, x, y, x1, y1);
+        
     }while(x < y);
 }
 int main(){
